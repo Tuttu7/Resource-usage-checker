4bc54ce2674021cdf46011a09ec58a294628780e
Tuttu7 <56312182+Tuttu7@users.noreply.github.com>
2021-02-13 19:31:51 +0530

    Add files via upload

---
+++ resource_usage_info.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+log_file=/home/tuttu/system.log
+
+date=$(date "+%d-%m-%Y %H:%M:%S")
+
+hostname=$(hostname)
+
+cpu_load=$( top -b -n 2 -d1 | grep "Cpu(s)"| tail -n1 | awk '{print $2}')
+
+load_average=$( uptime| awk '{print $7,$8,$9,$10}')
+
+memory_usage=$( free -mh | xargs | awk '{ print "Total/Free memory : " $8 " / " $12   }')
+
+echo "$date $hostname Load_average: $load_average CPU_load= $cpu_load Memory_usage: $memory_usage" >>  $log_file
+
+
+
