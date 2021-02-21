7f49a734d3d35ecae83454a2e45d30a92d8dc8a1
Tuttu7 <56312182+Tuttu7@users.noreply.github.com>
2021-02-21 16:58:47 +0530

    Update resource_usage_info.sh

--- resource_usage_info.sh
+++ resource_usage_info.sh
@@ -1,10 +1,7 @@
 
 
 
-
 #!/bin/bash
-
-log_file=/home/tuttu/system.log
 
 date=$(date "+%d-%m-%Y %H:%M:%S")
 
@@ -12,11 +9,6 @@ hostname=$(hostname)
 
 cpu_load=$( top -b -n 2 -d1 | grep "Cpu(s)"| tail -n1 | awk '{print $8}'| cut -c1-2)
 
-load_average=$( uptime| awk '{print $9,$10,$11}')
-
-memory_usage=$( free -mh | xargs | awk '{ print "Total/Free memory : " $8 " / " $12   }')
-
-echo "$date $hostname Load_average: $load_average CPU_load: $cpu_load Memory_usage: $memory_usage" >>  $log_file
 
 if [ $cpu_load -le 10 ]; then
 
